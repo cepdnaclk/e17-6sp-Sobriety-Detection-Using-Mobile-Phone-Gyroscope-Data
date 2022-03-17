6641b5e4a96e0ea5598292b698b42996d873e300
sathiiii <sathirasofte@gmail.com>
2022-03-17 08:31:51 +0530

    Create schema definitions for gyro and accelero data

---
+++ serverApp/models/acceleroReadings.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// Storing time-series data separately for I/O efficiency
+const acceleroReadingsSchema = new mongoose.Schema({
+    readings: [{timestamp: String, x: String, y: String, z: String}],
+    // Update readings at each T seconds (T=30s by default. But clients have the option to choose T depending on their resource limitations)
+    // However, since the data preprocessor and classifier expect readings of 10s windows, T being 2s, 5s, or any multiple of 10s is recommended
+    update_interval: {type: Number, default: 30},
+    user_ref: {type: mongoose.Schema.Types.ObjectId, ref: 'users'},
+    n_chuks: {type: Number, default: 0},          // Number of T seconds chuks in the readings
+}, {collection: 'acceleroReadings'});
+
+module.exports = mongoose.model('acceleroReadingsModel', acceleroReadingsSchema);
